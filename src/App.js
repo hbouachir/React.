2ef3cb62c816f2596b061dfc8013c6fd49ba44a7
hbouachir <bouachirhamza@gmail.com>
2020-05-22 10:17:12 +0100

    V0.5

--- src/App.js
+++ src/App.js
@@ -5,20 +5,67 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          
+ <div class="container">
+        <div class="row">
+            <div class="col-lg-3"></div>
+
+
+            <div class="col-lg-6">
+                  <div id="ui">
+                    <h1 class="text-center">Registration Form</h1>
+                      <form class="form-group text-center">
+                          <div class="row">
+                                <div class="col-lg-6">
+                                    <label>First Name:</label>
+                                    <input type="text" name="prenon" class="form-control" placeholder="Enter your first name...">
+                                </div>
+                                <div class="col-lg-6">
+                                  <label>Last Name:</label>
+                                  <input type="text" name="nom" class="form-control" placeholder="Enter your last name ...">
+                              </div>
+
+                          </div>
+                          <br>
+                          <label >Email:</label>
+                          <input type="email" name="email" class="form-control" placeholder="Enter your Email ...">
+                          <br>
+                          <div class="row">
+                            <div class="col-lg-6">
+                                <label>Password:</label>
+                                <input type="password" class="form-control" placeholder="Enter your password...">
+                            </div>
+                            <div class="col-lg-6">
+                              <label>Re-type password:</label>
+                              <input type="password"  class="form-control" placeholder="Re-type again ...">
+                          </div>
+
+                      </div>
+                      <br>
+                      <select class="form-control">
+                              <option >Choose gender ...</option>
+                              <option >Male</option>
+                              <option >Female</option>
+                      </select>
+                      <br>
+                      <input type="submit" name="submit" value="submit" class="btn btn-primary btn-block btn-lg">
+
+                      </form>
+
+
+                  </div>    
+
+            </div>
+
+
+
+
+            <div class="col-lg-3"></div>
+        </div>
+
+ </div>
+
+
     </div>
   );
 }
